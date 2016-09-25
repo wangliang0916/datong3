dedc680bc6b99c5857d230baf6929dacea224ecf
wl <wangliang0916@21cn.com>
2016-09-25 22:34:35 +0800

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Datong3::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datong3::Application.config.secret_token = 'b24d8835c988806b48f9906a7317e3fde9459b5788236efcc78fc0b522514cb5ee9ce526a5310c804daa6858d7d886458729b3c5f500da34f16a1f327ad28e2b'
